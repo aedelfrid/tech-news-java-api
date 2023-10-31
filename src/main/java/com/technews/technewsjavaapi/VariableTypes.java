a7a85c91745f6e8a608e2d00dc9ba778a61a21b2
aedelfrid <almck51@gmail.com>
2023-10-30 21:21:14 -0300

    added models

---
+++ src/main/java/com/technews/technewsjavaapi/VariableTypes.java
@@ -0,0 +1,16 @@
+package com.technews.technewsjavaapi;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 0.8;
+        String c = "JoeMama";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
